a1dfc589f10044ca7c360dec12936593b0731dd0
Patrick <patrickvongoeler@gmail.com>
2021-02-25 18:12:54 +0100

    add multiple files for same dataset

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,23 @@ import fs from "fs";
 
 function main() {
   const fileNames = getFiles("data");
+  const orderedFiles = [];
   for (const file of fileNames) {
     const [prefix, suffix] = file.split(".");
-    const raw = fs.readFileSync(`data/${file}`, {
+    const underscoreSplits = prefix.split("_");
+    const minText = underscoreSplits[underscoreSplits.length - 1];
+    const min = Number(minText.split("min")[0]);
+    orderedFiles.push({ min, name: file });
+  }
+  orderedFiles.sort((a, b) => a.min - b.min);
+
+  // keep these global to use max of only the first file
+  let yMax = Number.MIN_SAFE_INTEGER;
+  let yMin = Number.MAX_SAFE_INTEGER;
+  for (let j = 0; j < orderedFiles.length; j++) {
+    const file = orderedFiles[j];
+    const [prefix, suffix] = file.name.split(".");
+    const raw = fs.readFileSync(`data/${file.name}`, {
       encoding: "utf-8",
     });
     const [header, rest] = raw.split("XYDATA");
@@ -12,14 +26,15 @@ function main() {
     const data = rawData.trim();
     const list = data.split("\n");
 
-    let yMax = Number.MIN_SAFE_INTEGER;
-    let yMin = Number.MAX_SAFE_INTEGER;
-    // get min max to normalize
-    for (let i = 0; i < list.length; i++) {
-      const element = list[i];
-      const [x, y] = element.split("\t");
-      if (Number(y) > yMax) yMax = Number(y);
-      if (Number(y) < yMin) yMin = Number(y);
+    // only use first set to get min/max
+    if (j === 0) {
+      // get min max to normalize
+      for (let i = 0; i < list.length; i++) {
+        const element = list[i];
+        const [x, y] = element.split("\t");
+        if (Number(y) > yMax) yMax = Number(y);
+        if (Number(y) < yMin) yMin = Number(y);
+      }
     }
 
     let output = "";
